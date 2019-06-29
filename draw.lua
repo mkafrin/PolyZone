28d92abcf7e162a6edef28c5712592953f708b3f
Russian <russianronin@roninnetwork.com>
2019-06-28 18:12:15 -0700

    Fix draw bug
    Co-authored-by: mkafrin <mafrin@email.sc.edu>

--- draw.lua
+++ draw.lua
@@ -3,11 +3,11 @@ local function _draw(shape, opt)
   local zDrawDist = 75.0
   local plyPed = PlayerPedId()
   local plyPos = GetEntityCoords(plyPed)
-  local minZ = shape.minZ or plyPos.z - zDrawDist
-  local maxZ = shape.maxZ or plyPos.z + zDrawDist
+  local minZ = shape.minZ ~= math.mininteger and shape.minZ or plyPos.z - zDrawDist
+  local maxZ = shape.maxZ ~= math.maxinteger and shape.maxZ or plyPos.z + zDrawDist
   for i=1, #shape.points do
     DrawLine(shape.points[i].x, shape.points[i].y, minZ, shape.points[i].x, shape.points[i].y, maxZ, 255, 0, 0, 255)
-    if #shape.points >= i+1 then
+    if i < #shape.points then
       for j = minZ, maxZ, opt.lineSepDist or 5.0 do
         DrawLine(shape.points[i].x, shape.points[i].y, j, shape.points[i+1].x, shape.points[i+1].y, j, 0, 255, 0, 255)
       end
