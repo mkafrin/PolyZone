fc19caa4a2515006c6c0ff82a2aac9147f4ab50a
mkafrin <1360790+mkafrin@users.noreply.github.com>
2020-04-18 19:49:52 -0400

    Replaced __resource.lua with fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+games {'gta5'}
+
+fx_version 'bodacious'
+
+description 'Define polygonal zones and test whether a point is inside or outside of the zone'
+version '1.0.0'
+
+client_scripts {
+  'client.lua',
+  'commands.lua',
+  'creation.lua'
+}
+
+server_scripts {
+  'server.lua'
+}
