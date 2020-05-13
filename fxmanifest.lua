0a124323b41b99af9d76c4bd5e4e6254cdfc5ab3
Russian <russianronin@roninnetwork.com>
2020-05-13 13:57:27 -0700

    update version in mainfest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games {'gta5'}
 fx_version 'bodacious'
 
 description 'Define polygonal zones and test whether a point is inside or outside of the zone'
-version '1.0.0'
+version '1.1.0'
 
 client_scripts {
   'client.lua',
