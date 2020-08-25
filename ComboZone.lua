3a46c1675b8bc4b9d8556b7716a4c2b5aca11dcc
mkafrin <1360790+mkafrin@users.noreply.github.com>
2020-08-25 00:49:47 -0400

    ComboZone: Separated exhaustive InOut helpers to their own function
    The exhaustive version of isPointInside, onPointInOut, and onPlayerInOut had a bug due to the way the "exhaustiveness" was checked. Checking this in the proper way would have made these methods even more complicated. Also due to the way this function was responsible for exhaustive and non-exhaustive zones, even when exhaustive was set to false, it would drop performance and cause memory churn on the ComboZone.
    
    To fix these issues, I pulled out exhaustive from being a parameter of these functions to having entirely separate functions. Now, if you want to do an exhaustive isPointInside, you call isPointInsideExhaustive(). Same for onPointInOut and onPlayerInOut. This improves performance and fixed the bug.

--- ComboZone.lua
+++ ComboZone.lua
@@ -1,5 +1,19 @@
 ComboZone = {}
 
+local function _areInsideZonesTablesEqual(tblA, tblB)
+  if tblA == nil or tblB == nil or #tblA ~= #tblB then
+    return false
+  end
+  for i=1, #tblA do
+    local a = tblA[i]
+    local b = tblB[i]
+    if a ~= nil and b ~= nil and a.id ~= b.id then
+      return false
+    end
+  end
+  return true
+end
+
 function ComboZone:draw()
   local zones = self.zones
   for i=1, #zones do
@@ -26,6 +40,13 @@ end
 
 function ComboZone:new(zones, options)
   options = options or {}
+  -- Add a unique id for each zone in the ComboZone
+  for i=1, #zones do
+    local zone = zones[i]
+    if zone then
+      zone.id = i
+    end
+  end
   local zone = {
     name = tostring(options.name) or nil,
     zones = zones,
@@ -43,26 +64,37 @@ function ComboZone:Create(zones, options)
   return zone
 end
 
-function ComboZone:isPointInside(point, exhaustive)
+function ComboZone:isPointInside(point)
   if self.destroyed then
     print("[PolyZone] Warning: Called isPointInside on destroyed zone {name=" .. self.name .. "}")
     return false, {}
   end
 
-  local isInside = false
+  local zones = self.zones
+  for i=1, #zones do
+    local zone = zones[i]
+    if zone and zone:isPointInside(point) then
+      return true, zone
+    end
+  end
+  return false, nil
+end
+
+function ComboZone:isPointInsideExhaustive(point)
+  if self.destroyed then
+    print("[PolyZone] Warning: Called isPointInside on destroyed zone {name=" .. self.name .. "}")
+    return false, {}
+  end
+
   local insideZones = {}
   local zones = self.zones
   for i=1, #zones do
     local zone = zones[i]
     if zone and zone:isPointInside(point) then
-      isInside = true
       insideZones[#insideZones+1] = zone
-      if not exhaustive then
-        break
-      end
     end
   end
-  return isInside, insideZones
+  return #insideZones ~= 0, insideZones
 end
 
 function ComboZone:destroy()
@@ -76,23 +108,22 @@ function ComboZone:destroy()
   end
 end
 
-function ComboZone:onPointInOut(getPointCb, onPointInOutCb, waitInMS, exhaustive)
+function ComboZone:onPointInOut(getPointCb, onPointInOutCb, waitInMS)
   -- Localize the waitInMS value for performance reasons (default of 500 ms)
   local _waitInMS = 500
   if waitInMS ~= nil then _waitInMS = waitInMS end
 
   Citizen.CreateThread(function()
     local isInside = nil
-    local insideZonesCount = 0
+    local insideZone = nil
     while not self.destroyed do
       if not self.paused then
         local point = getPointCb()
-        local newIsInside, insideZones = self:isPointInside(point, exhaustive)
-        local newInsideZonesCount = #insideZones
-        if newIsInside ~= isInside or newInsideZonesCount ~= insideZonesCount then
-          onPointInOutCb(newIsInside, point, insideZones)
+        local newIsInside, newInsideZone = self:isPointInside(point)
+        if newIsInside ~= isInside then
+          onPointInOutCb(newIsInside, point, newInsideZone or insideZone)
           isInside = newIsInside
-          insideZonesCount = newInsideZonesCount
+          insideZone = newInsideZone
         end
       end
       Citizen.Wait(_waitInMS)
@@ -100,8 +131,35 @@ function ComboZone:onPointInOut(getPointCb, onPointInOutCb, waitInMS, exhaustive
   end)
 end
 
-function ComboZone:onPlayerInOut(onPointInOutCb, waitInMS, exhaustive)
-  self:onPointInOut(PolyZone.getPlayerPosition, onPointInOutCb, waitInMS, exhaustive)
+function ComboZone:onPointInOutExhaustive(getPointCb, onPointInOutCb, waitInMS)
+  -- Localize the waitInMS value for performance reasons (default of 500 ms)
+  local _waitInMS = 500
+  if waitInMS ~= nil then _waitInMS = waitInMS end
+
+  Citizen.CreateThread(function()
+    local isInside = nil
+    local insideZones = {}
+    while not self.destroyed do
+      if not self.paused then
+        local point = getPointCb()
+        local newIsInside, newInsideZones = self:isPointInsideExhaustive(point)
+        if newIsInside ~= isInside or not _areInsideZonesTablesEqual(insideZones, newInsideZones)  then
+          onPointInOutCb(newIsInside, point, newInsideZones)
+          isInside = newIsInside
+          insideZones = newInsideZones
+        end
+      end
+      Citizen.Wait(_waitInMS)
+    end
+  end)
+end
+
+function ComboZone:onPlayerInOut(onPointInOutCb, waitInMS)
+  self:onPointInOut(PolyZone.getPlayerPosition, onPointInOutCb, waitInMS)
+end
+
+function ComboZone:onPlayerInOutExhaustive(onPointInOutCb, waitInMS)
+  self:onPointInOutExhaustive(PolyZone.getPlayerPosition, onPointInOutCb, waitInMS)
 end
 
 function ComboZone:setPaused(paused)
