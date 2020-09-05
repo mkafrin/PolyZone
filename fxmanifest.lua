552a5cd0462180523adee05e8c4b4a941412b910
mkafrin <1360790+mkafrin@users.noreply.github.com>
2020-09-05 11:16:05 -0400

    fxmanifest.lua: Update version number for release

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ games {'gta5'}
 
 fx_version 'bodacious'
 
-description 'Define polygonal zones and test whether a point is inside or outside of the zone'
-version '2.1.0'
+description 'Define zones of different shapes and test whether a point is inside or outside of the zone'
+version '2.2.0'
 
 client_scripts {
   'client.lua',
