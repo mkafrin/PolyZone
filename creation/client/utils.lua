0dfe9e63333f26204c21ea66284602b95d4b71b4
Spudgun <jared.rj.white@gmail.com>
2022-04-15 10:33:07 +1200

    fix: drawZone check for disabled key input

--- creation/client/utils.lua
+++ creation/client/utils.lua
@@ -57,21 +57,19 @@ function handleArrowInput(center, heading)
 end
 
 function disableControlKeyInput()
-  if not drawZone then
-    Citizen.CreateThread(function()
-      while drawZone do
-        DisableControlAction(0, 36, true)   -- Ctrl
-        DisableControlAction(0, 19, true)   -- Alt
-        DisableControlAction(0, 20, true)   -- 'Z'
-        DisableControlAction(0, 21, true)   -- Shift
-        DisableControlAction(0, 81, true)   -- Scroll Wheel Down
-        DisableControlAction(0, 99, true)   -- Scroll Wheel Up
-        DisableControlAction(0, 172, true)  -- Arrow Up
-        DisableControlAction(0, 173, true)  -- Arrow Down
-        DisableControlAction(0, 174, true)  -- Arrow Left
-        DisableControlAction(0, 175, true)  -- Arrow Right
-        Wait(0)
-      end
-    end)
-  end
+  Citizen.CreateThread(function()
+    while drawZone do
+      DisableControlAction(0, 36, true)   -- Ctrl
+      DisableControlAction(0, 19, true)   -- Alt
+      DisableControlAction(0, 20, true)   -- 'Z'
+      DisableControlAction(0, 21, true)   -- Shift
+      DisableControlAction(0, 81, true)   -- Scroll Wheel Down
+      DisableControlAction(0, 99, true)   -- Scroll Wheel Up
+      DisableControlAction(0, 172, true)  -- Arrow Up
+      DisableControlAction(0, 173, true)  -- Arrow Down
+      DisableControlAction(0, 174, true)  -- Arrow Left
+      DisableControlAction(0, 175, true)  -- Arrow Right
+      Wait(0)
+    end
+  end)
 end
